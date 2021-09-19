8ca4ebe1b6d8e51dfbfa9f429eb5805a1d79dc59
Aryqs-Ipsum <mrfeyurikenpro@gmail.com>
2021-09-19 18:49:32 +0200

    update colors

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Arno Cellarier.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quidem odio officia sit eveniet reprehenderit aut eaque! Modi, quos? Enim at dignissimos tempore excepturi accusantium? Magnam dolorum praesentium suscipit iure!</p>
     </div>
 </template>
 
